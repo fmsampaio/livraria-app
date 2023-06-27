2e93513f0e593eaca76ed174c6f0c567a1934c4d
Felipe Sampaio <felipe.sampaio@farroupilha.ifrs.edu.br>
2023-06-27 14:41:25 -0300

    Setting up heroky postgress DB

--- livraria_app/settings.py
+++ livraria_app/settings.py
@@ -13,8 +13,12 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 import os
 
+import dj_database_url
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+IS_HEROKU_APP = "DYNO" in os.environ and not "CI" in os.environ
 
 
 # Quick-start development settings - unsuitable for production
@@ -86,12 +90,23 @@ WSGI_APPLICATION = 'livraria_app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if IS_HEROKU_APP:
+
+    DATABASES = {
+            "default" : dj_database_url.config(
+                conn_max_age=600,
+                conn_health_checks=True,
+                ssl_require=True,
+            )
     }
-}
+else:
+    
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
